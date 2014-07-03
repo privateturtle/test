be0a93152d622bd9f6de8dff16e3193370b69427
Peter Szafarz <peter@Peter-Szafarz-iMac.local>
2014-07-03 17:49:06 +0200

    + rename test1.py in dic_to_json

---
+++ dict_to_json.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+ 
+# importiert das json-Modul
+import json
+
+# Daten mit dicts und lists erfassen
+daten = dict()
+daten["herausgeber"] = "Xema"
+daten["Nummer"] = "1234-5678-9012-3456"
+daten["Deckung"] = 2e+6
+daten["Währung"] = "EURO"
+inhaber = dict()
+inhaber["Name"] = "Mustermann"
+inhaber["Name"] = "Max"
+inhaber["männlich"] = True
+hobbys = list()
+hobbys.append("Reiten")
+hobbys.append("Golfen")
+hobbys.append("Lesen")
+inhaber["Hobbys"] = hobbys
+inhaber["Alter"] = 42
+inhaber["Kinder"] = list()
+inhaber["Partner"] = None
+daten["Inhaber"] = inhaber
+
+# Daten serialisieren
+zeichenkette = json.dumps(daten, indent=4)
+ 
+# ... irgendwann an anderer Stelle ... 
+
+new_file = file('json.txt', 'a')
+new_file.write(zeichenkette + "\n")
+new_file.close()
+
+# Daten deserialisieren
+# erhalten = json.loads(zeichenkette)
+
+# und ausgeben
+# print erhalten
